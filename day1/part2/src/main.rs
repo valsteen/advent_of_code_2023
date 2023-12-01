a29d1e733f269e886eab9914c691c85443f27af8
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-01 20:57:48 +0100

    better error reporting

--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -12,17 +12,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let matcher = AhoCorasick::new(numbers)?;
 
     let lines = stdin().lock().lines();
-    let sum = lines
-        .flatten()
-        .try_fold(0, |acc, line| {
-            let mut patterns = matcher.find_overlapping_iter::<&[u8]>(line.as_ref());
-            let first = patterns.next()?.pattern().as_usize() % 9 + 1;
-            let last = patterns
-                .last()
-                .map_or(first, |found| found.pattern().as_usize() % 9 + 1);
-            Some(acc + first * 10 + last)
-        })
-        .ok_or("invalid input")?;
+    let sum = lines.flatten().try_fold(0, |acc, line| {
+        let mut patterns = matcher.find_overlapping_iter::<&[u8]>(line.as_ref());
+        let first = patterns
+            .next()
+            .ok_or_else(|| format!("No match in '{line}'"))?
+            .pattern()
+            .as_usize()
+            % 9
+            + 1;
+        let last = patterns
+            .last()
+            .map_or(first, |found| found.pattern().as_usize() % 9 + 1);
+        Ok::<_, String>(acc + first * 10 + last)
+    })?;
 
     println!("{sum}");
     Ok(())
