8351332e4919f98acb2daeb9717be660afe98979
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-02 14:52:50 +0100

    rewrite without external crate

--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -1,29 +1,57 @@
-use aho_corasick::AhoCorasick;
 use std::error::Error;
 use std::io::{stdin, BufRead};
 
 const VALID_NUMBERS: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let numbers = (1..=9)
-        .map(|i| i.to_string().as_bytes().to_owned())
-        .chain(VALID_NUMBERS.into_iter().map(|number| number.as_bytes().to_owned()));
+struct FoundString {
+    len: usize,
+    index: usize,
+}
 
-    let matcher = AhoCorasick::new(numbers)?;
+fn find_all<T: AsRef<[u8]>, I: AsRef<[u8]>, U: AsRef<[I]>>(needles: U, haystack: T) -> Vec<usize> {
+    let needles = needles.as_ref();
+    let mut progress = Vec::<FoundString>::new();
+    let mut result = Vec::new();
+
+    for c in haystack.as_ref() {
+        progress.retain_mut(|found| {
+            let needle = needles[found.index].as_ref();
+            if needle[found.len] == *c {
+                found.len += 1;
+                if found.len == needle.len() {
+                    result.push(found.index);
+                    false
+                } else {
+                    true
+                }
+            } else {
+                false
+            }
+        });
+        for (index, needle) in needles.iter().map(AsRef::as_ref).enumerate() {
+            if &needle[0] == c {
+                if needle.len() == 1 {
+                    result.push(index);
+                } else {
+                    progress.push(FoundString { len: 1, index });
+                }
+            }
+        }
+    }
+    result
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let numbers: Vec<Vec<u8>> = (1..=9)
+        .map(|i| i.to_string().as_bytes().to_owned())
+        .chain(VALID_NUMBERS.into_iter().map(|number| number.as_bytes().to_owned()))
+        .collect();
 
     let lines = stdin().lock().lines();
     let sum = lines.flatten().try_fold(0, |acc, line| {
-        let mut patterns = matcher.find_overlapping_iter::<&[u8]>(line.as_ref());
-        let first = patterns
-            .next()
-            .ok_or_else(|| format!("No match in '{line}'"))?
-            .pattern()
-            .as_usize()
-            % 9
-            + 1;
-        let last = patterns
-            .last()
-            .map_or(first, |found| found.pattern().as_usize() % 9 + 1);
+        let mut patterns = find_all(&numbers, line.as_bytes()).into_iter();
+        let first = patterns.next().ok_or_else(|| format!("No match in '{line}'"))? % 9 + 1;
+        let last = patterns.last().map_or(first, |found| found % 9 + 1);
         Ok::<_, String>(acc + first * 10 + last)
     })?;
 
