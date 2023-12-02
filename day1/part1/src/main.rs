4ec292b0020d72356d733583042e3b3fffacda45
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-02 01:52:17 +0100

    better error reporting

--- day1/part1/src/main.rs
+++ day1/part1/src/main.rs
@@ -3,14 +3,11 @@ use std::io::{stdin, BufRead};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
-    let sum = lines
-        .flatten()
-        .try_fold(0, |acc, line| {
-            let mut digits = line.chars().filter_map(|c| c.to_digit(10));
-            let first = digits.next()?;
-            Some(acc + first * 10 + digits.last().unwrap_or(first))
-        })
-        .ok_or("invalid input")?;
+    let sum = lines.flatten().try_fold(0, |acc, line| {
+        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+        let first = digits.next().ok_or(format!("No match in '{line}'"))?;
+        Ok::<_, String>(acc + first * 10 + digits.last().unwrap_or(first))
+    })?;
 
     println!("{sum}");
     Ok(())
