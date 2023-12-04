f4f0068470808e9fe5c8efae2f9486e2c0df42be
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-04 19:10:39 +0100

    day 4 part 2

--- day4/part2/src/main.rs
+++ day4/part2/src/main.rs
@@ -1,47 +1,50 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 use std::error::Error;
 use std::io::{stdin, BufRead};
+use std::iter::repeat;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = stdin().lock().lines();
 
-    let mut winning = HashMap::new();
-    let mut hands = VecDeque::new();
-    let mut n = 1;
+    let (_, score) = lines
+        .map_while(Result::ok)
+        .try_fold((VecDeque::new(), 0), |(mut copies, total), line| {
+            let instances = copies.pop_front().unwrap_or(1);
 
-    for line in lines.map_while(Result::ok) {}.try_fold(VecDeque::new(), |mut hands, (n, line)| {
-        let (_, line) = line.split_once(':').ok_or_else(|| format!("Malformed line: {line}"))?;
-        let (part1, part2) = line.split_once('|').ok_or_else(|| format!("Malformed line: {line}"))?;
+            let (_, line) = line.split_once(':').ok_or_else(|| format!("Malformed line: {line}"))?;
+            let (part1, part2) = line.split_once('|').ok_or_else(|| format!("Malformed line: {line}"))?;
 
-        let winning = part1
-            .split(' ')
-            .filter(|s| !s.is_empty())
-            .map(str::parse::<u64>)
-            .collect::<Result<HashSet<_>, _>>()
-            .map_err(|_| format!("Malformed line: {line}"))?;
-        let score = part2
-            .split(' ')
-            .filter(|s| !s.is_empty())
-            .map(str::parse::<u64>)
-            .try_fold(None, |acc, n| {
-                let n = n.map_err(|_| format!("Malformed line: {line}"))?;
-                Ok::<_, Box<dyn Error>>(
-                    winning
-                        .contains(&n)
-                        .then(|| Some(acc.map_or(1, |acc| acc * 2)))
-                        .unwrap_or(acc),
-                )
-            })?
-            .unwrap_or_default();
-        if score > 0 {
-            hands.push_back((n + 1, score));
-        }
-        Ok::<_, Box<dyn Error>>(hands)
-    })?;
-    let mut wins = HashMap::new();
+            let winning = part1
+                .split(' ')
+                .filter(|s| !s.is_empty())
+                .map(str::parse::<u64>)
+                .collect::<Result<HashSet<_>, _>>()
+                .map_err(|_| format!("Malformed line: {line}"))?;
+            let score = part2
+                .split(' ')
+                .filter(|s| !s.is_empty())
+                .map(str::parse::<u64>)
+                .try_fold(0, |acc, n| {
+                    let n = n.map_err(|_| format!("Malformed line: {line}"))?;
+                    if winning.contains(&n) {
+                        Ok::<_, Box<dyn Error>>(acc + 1)
+                    } else {
+                        Ok(acc)
+                    }
+                })?;
 
-    while let Some(hand) = hands.pop_front() {}
-
-    println!("{hands:?}");
+            let mut index = 0;
+            while index < score {
+                if let Some(n) = copies.get_mut(index) {
+                    *n += instances;
+                    index += 1;
+                    continue;
+                }
+                break;
+            }
+            copies.extend(repeat(1 + instances).take(score - index));
+            Ok::<_, Box<dyn Error>>((copies, instances + total))
+        })?;
+    println!("{score}");
     Ok(())
 }
