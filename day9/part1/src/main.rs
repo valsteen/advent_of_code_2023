6beadc774b6798ce5ce132cfb8e1212fc180005d
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-09 11:48:50 +0100

    pass vec by value, making the recursion as cheap as a loop

--- day9/part1/src/main.rs
+++ day9/part1/src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::io::{stdin, BufRead};
+use std::ops::Deref;
 
-fn resolve(numbers: &[i64]) -> Option<i64> {
+fn resolve<T: Deref<Target = [i64]>>(numbers: T) -> Option<i64> {
     Some(
         *numbers.last()?
             + match (0..numbers.len() - 1).fold(
@@ -14,7 +15,7 @@ fn resolve(numbers: &[i64]) -> Option<i64> {
                 },
             ) {
                 (true, _) => 0,
-                (_, next) => resolve(next.as_slice())?,
+                (_, next) => resolve(next)?,
             },
     )
 }
@@ -24,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let sum = lines
         .map(|line| {
             let numbers = line?.split(' ').map(str::parse::<i64>).collect::<Result<Vec<_>, _>>()?;
-            Ok(resolve(numbers.as_slice()).ok_or("cannot compute next number")?)
+            Ok(resolve(numbers).ok_or("cannot compute next number")?)
         })
         .sum::<Result<i64, Box<dyn Error>>>()?;
 
--- day9/part2/src/main.rs
+++ day9/part2/src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::io::{stdin, BufRead};
+use std::ops::Deref;
 
-fn resolve(numbers: &[i64]) -> Option<i64> {
+fn resolve<T: Deref<Target = [i64]>>(numbers: T) -> Option<i64> {
     Some(
         *numbers.last()?
             + match (0..numbers.len() - 1).fold(
@@ -14,7 +15,7 @@ fn resolve(numbers: &[i64]) -> Option<i64> {
                 },
             ) {
                 (true, _) => 0,
-                (_, next) => resolve(next.as_slice())?,
+                (_, next) => resolve(next)?,
             },
     )
 }
@@ -27,7 +28,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .rsplit(' ')
                 .map(str::parse::<i64>)
                 .collect::<Result<Vec<_>, _>>()?;
-            Ok(resolve(numbers.as_slice()).ok_or("cannot compute next number")?)
+            Ok(resolve(numbers).ok_or("cannot compute next number")?)
         })
         .sum::<Result<i64, Box<dyn Error>>>()?;
 
