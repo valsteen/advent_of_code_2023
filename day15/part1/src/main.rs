dc49f7bfbc4accfbb68d5ec692806b9431f575b6
Vincent Alsteen <vincent.alsteen@gmail.com>
2023-12-16 14:05:02 +0100

    day 15 part 1

---
+++ day15/part1/src/main.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::io::{stdin, BufRead};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sum = stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            Ok(line?
+                .split(',')
+                .map(|s| {
+                    println!("{s}");
+                    let mut result = 0usize;
+
+                    for c in s.bytes() {
+                        result += usize::from(c);
+                        result *= 17;
+                        result %= 256;
+                    }
+                    result
+                })
+                .sum::<usize>())
+        })
+        .sum::<Result<usize, Box<dyn Error>>>()?;
+
+    println!("{sum}");
+
+    Ok::<_, Box<dyn Error>>(())
+}
